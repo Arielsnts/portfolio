2852ca7bb3142795710552e67cb356f4146f82a8
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2025-11-22 15:35:15 -0300

    api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://portifolio-puce-nine-15.vercel.app/",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
